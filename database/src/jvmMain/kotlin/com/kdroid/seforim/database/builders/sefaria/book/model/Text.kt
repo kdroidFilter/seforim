5c3560b22fabec7286bc62160de248f0ffcea96b
Elie G <elyahou.hadass@gmail.com>
2024-12-19 08:24:12 +0200

    Update comments in Text.kt for clarity and consistency
    Replaced French comments with English equivalents to improve code readability and maintain consistency across the project. Adjusted the description for the `text` field to clarify its functionality.

--- database/src/jvmMain/kotlin/com/kdroid/seforim/database/builders/sefaria/book/model/Text.kt
+++ database/src/jvmMain/kotlin/com/kdroid/seforim/database/builders/sefaria/book/model/Text.kt
@@ -3,12 +3,12 @@ package com.kdroid.seforim.database.builders.sefaria.book.model
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 
-// Structure retournée par l'endpoint texte
+// Structure returned by the text endpoint
 @Serializable
 internal data class Version(
     val status: String? = null,
     val language: String? = null,
-    val text: JsonElement, // Changement ici pour gérer JsonPrimitive et JsonArray
+    val text: JsonElement, // Handling JsonPrimitive and JsonArray
     val versionSource: String? = null
 )
 
